99aa9e651ee1c5f28442938230355083e8cb2416
Matt <amsellem.matt@gmail.com>
2019-11-26 08:27:02 +0100

    Added first part of conversion function

--- calculatrice.py
+++ calculatrice.py
@@ -1,5 +1,27 @@
-def conversion(chaine):
-    pass
-
 def symboleI():
     return 1
+
+def symboleV():
+    return 5
+
+def symboleX():
+    return 10
+
+def symboleL():
+    return 50
+
+def symboleC():
+    return 100
+
+def symboleD():
+    return 500
+
+def symboleM():
+    return 1000
+
+def conversion(chaine):
+    somme = 0
+    for i in range(0,len(chaine)):
+        if chaine[i] == 'M':
+            somme += symboleM()
+    return somme
--- test_calculatrice.py
+++ test_calculatrice.py
@@ -1,10 +1,25 @@
 from calculatrice import *
 
-def test_conversion():
-    pass
-
 def test_symboleI():
     assert symboleI() == 1
 
 def test_symboleV() :
     assert symboleV() == 5
+
+def test_symboleX() :
+    assert symboleX() == 10
+
+def test_symboleL() :
+    assert symboleL() == 50
+
+def test_symboleC() :
+    assert symboleC() == 100
+
+def test_symboleD() :
+    assert symboleD() == 500
+
+def test_symboleM():
+    assert symboleM() == 1000
+
+def test_conversion():
+    assert conversion("M") == 1000
