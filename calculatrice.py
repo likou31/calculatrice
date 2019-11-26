cd6d83783ecfe8948a3b25c016ce760239579096
Matt <amsellem.matt@gmail.com>
2019-11-26 09:43:31 +0100

    Added test function for division

--- calculatrice.py
+++ calculatrice.py
@@ -63,4 +63,4 @@ def multiplication_nombre_romain(nombre1, nombre2):
     return conversion(nombre1) * conversion(nombre2)
 
 def division_nombre_romain(nombre1, nombre2):
-    pass
+    return conversion(nombre1) / conversion(nombre2)
