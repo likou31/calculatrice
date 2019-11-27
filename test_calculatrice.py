33b5f23b5170c3c1149865371a850cf2f7d03d16
Matt <amsellem.matt@gmail.com>
2019-11-27 08:00:03 +0100

    Added mocked test for multiplication and division

--- test_calculatrice.py
+++ test_calculatrice.py
@@ -58,6 +58,10 @@ def test_calculatrice(monkeypatch):
     assert calculatrice.calculatrice('+', "III", "IV") == 10
     monkeypatch.setattr(calculatrice, 'soustraction_nombre_romain', mocked_soustraction_nombre_romain)
     assert calculatrice.calculatrice('-', "L", "X") == 36
+    monkeypatch.setattr(calculatrice, 'multiplication_nombre_romain', mocked_multiplication_nombre_romain)
+    assert calculatrice.calculatrice('*', "V", "IV") == 24
+    monkeypatch.setattr(calculatrice, 'division_nombre_romain', mocked_division_nombre_romain)
+    assert calculatrice.calculatrice('/', "XV", "V") == 2
 
 
 
@@ -66,3 +70,9 @@ def mocked_addition_nombre_romain(nombre1, nombre2):
 
 def mocked_soustraction_nombre_romain(nombre1, nombre2):
     return conversion(nombre1) - conversion(nombre2) - 4
+
+def mocked_multiplication_nombre_romain(nombre1, nombre2):
+    return conversion(nombre1) * conversion(nombre2) + 4
+
+def mocked_division_nombre_romain(nombre1, nombre2):
+    return conversion(nombre1) / conversion(nombre2) -1
